ed731020f8ba52857a212a6b0f5870df1db46349
Serhii FSI <fsitmp@gmail.com>
2025-02-15 03:13:14 +0000

    Add component and refactor

--- entrypoints/content/modifiers/TaskDeleteMod.tsx
+++ entrypoints/content/modifiers/TaskDeleteMod.tsx
@@ -1,101 +1,116 @@
-import ReactDOM from "react-dom/client";
 import { useState } from "react";
+import ReactDOM from "react-dom/client";
 
 import { Modifier } from "./Modifier";
 
 import "./TaskDeleteMod.css";
 
+function Component({ parent }: { parent: any }) {
+  const [isDelete, setIsDelete] = useState(false);
+
+  return isDelete ? (
+    <>
+      <button
+        className="TaskDeleteMod_Button"
+        onClick={() => {
+          setIsDelete(false);
+          parent.query();
+        }}
+      >
+        Yes
+      </button>
+      <button
+        className="TaskDeleteMod_Button"
+        onClick={() => {
+          setIsDelete(false);
+        }}
+      >
+        Cancel
+      </button>
+    </>
+  ) : (
+    <button
+      className="TaskDeleteMod_Button"
+      onClick={() => {
+        setIsDelete(true);
+      }}
+    >
+      Delete
+    </button>
+  );
+}
+
 export class TaskDeleteMod extends Modifier {
+  dialogWrapper: HTMLElement | null | undefined;
+  actionsWrapper: HTMLElement | null | undefined;
+
   execute() {
     this.observerCallback = this.add;
   }
 
-  checkIsTaskInDom() {
-    return Boolean(
-      document.querySelector(
-        ".el-dialog__wrapper:has(.task-wrapper):has(.task--name):has(.task--tasked>a)"
-      )
+  getDialogWrapper() {
+    this.dialogWrapper = document.querySelector(
+      ".el-dialog__wrapper:has(.task-wrapper):has(.task--name):has(.task--tasked>a)"
+    ) as HTMLElement;
+  }
+
+  getActionsWrapper() {
+    this.actionsWrapper = this.dialogWrapper?.querySelector(
+      ".task-footer>.actions"
     );
+  }
+
+  checkIsTaskInDom() {
+    return Boolean(this.dialogWrapper);
   }
 
   checkIsTaskOpened() {
-    const dialogWrapper: HTMLElement | null = document.querySelector(
-      ".el-dialog__wrapper:has(.task-wrapper):has(.task--name):has(.task--tasked>a)"
-    );
-    return dialogWrapper?.style.display !== "none";
+    return this.dialogWrapper?.style.display !== "none";
   }
 
   checkIsButtonAdded() {
-    return false;
+    return Boolean(this.dialogWrapper?.querySelector(".TaskDeleteMod_Button"));
   }
 
   getTaskName() {
-    const el = document
-      .querySelector(
-        ".el-dialog__wrapper:has(.task-wrapper):has(.task--name):has(.task--tasked>a)"
-      )
-      ?.querySelector(".task--name");
+    const el = this.dialogWrapper?.querySelector(".task--name");
     return el?.textContent?.trim();
   }
 
   getPipelineName() {
-    const el = document
-      .querySelector(
-        ".el-dialog__wrapper:has(.task-wrapper):has(.task--name):has(.task--tasked>a)"
-      )
-      ?.querySelector(".task--tasked>a");
+    const el = this.dialogWrapper?.querySelector(".task--tasked>a");
     return el?.textContent?.trim();
   }
 
-  add() {
-    if (!this.checkIsTaskInDom()) return;
-    if (!this.checkIsTaskOpened()) return;
-    if (this.checkIsButtonAdded()) return;
-
-    console.log("Do JOB!");
+  query() {
+    this.getDialogWrapper();
+    console.log("Do Query!");
     console.log(this.getTaskName());
     console.log(this.getPipelineName());
   }
+
+  addButton() {
+    const ui = createIntegratedUi(this.ctx, {
+      position: "inline",
+      anchor: this.actionsWrapper,
+      onMount: (container) => {
+        const root = ReactDOM.createRoot(container);
+        root.render(<Component parent={this} />);
+        return root;
+      },
+      onRemove: (root) => {
+        root?.unmount();
+      },
+    });
+    ui.mount();
+  }
+
+  add() {
+    this.getDialogWrapper();
+    if (!this.checkIsTaskInDom()) return;
+    if (!this.checkIsTaskOpened()) return;
+    if (this.checkIsButtonAdded()) return;
+    this.getActionsWrapper();
+    this.addButton();
+  }
 }
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   const ui = createIntegratedUi(ctx, {
-//     position: "inline",
-//     anchor: "body",
-//     onMount: (container) => {
-//       // Create a root on the UI container and render a component
-//       const root = ReactDOM.createRoot(container);
-//       renderApp(root);
-//       return root;
-//     },
-//     onRemove: (root) => {
-//       // Unmount the root when the UI is removed
-//       root?.unmount();
-//     },
-//   });
-//   // Call mount to add the UI to the DOM
-//   ui.mount();
-
-//   return (
-//     <>
-//       <h1>WXT + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the WXT and React logos to learn more
-//       </p>
-//     </>
-//   );
-// }
-
-// function renderApp(root: ReactDOM.Root) {
-//   root.render(<App />);
-// }
